25e1f0a100157a7a2d7927a1e161bc03edf33ebb
David Barratt <dbarratt@wikimedia.org>
2017-10-31 14:51:02 -0400

    Fix property name

--- src/components/fields/select-wiki.container.js
+++ src/components/fields/select-wiki.container.js
@@ -6,7 +6,7 @@ import SelectWiki from './select-wiki';
 
 export default connect(
 	state => ( {
-		loading: !state.wikis.size,
+		isLoading: !state.wikis.size,
 		value: state.query.wiki,
 		options: WikiSelectors.getWikiOptions( state )
 	} ),
