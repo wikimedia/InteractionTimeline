e4ff2cf390980448f0a0515c60582ebb87217e29
David Barratt <dbarratt@wikimedia.org>
2017-11-01 20:04:06 -0400

    Begin styling the timeline

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import Form from './form/form';
+import Form from './form';
+import Timeline from './timeline';
 
 export default () => (
 	<div className="container">
-		<div className="row">
+		<div className="row mb-3">
 			<div className="col">
 				<h2><FormattedMessage id="app-title" /></h2>
 			</div>
 		</div>
-		<div className="row">
+		<div className="row mb-3">
 			<div className="col">
 				<Form />
+			</div>
+		</div>
+		<div className="row">
+			<div className="col">
+				<Timeline />
 			</div>
 		</div>
 	</div>
