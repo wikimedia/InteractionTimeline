57343276b111c790ebb5d3e5dd9d4ce72925ba48
David Barratt <dbarratt@wikimedia.org>
2017-11-06 09:52:31 -0500

    Limit users to 2

--- src/reducers/query.js
+++ src/reducers/query.js
@@ -3,9 +3,21 @@ import Query from 'app/entities/query';
 export default ( state = new Query(), action ) => {
 	switch ( action.type ) {
 		case 'QUERY_UPDATE':
-			return action.query;
+			let query = action.query;
+
+			if ( query.user ) {
+				query = query.set( 'user', query.user.slice( 0, 2 ) );
+			}
+
+			return query;
 		case 'QUERY_SET_VALUE':
-			return state.set( action.key, action.value );
+			let value = action.value;
+
+			if ( action.key === 'user' ) {
+				value = value.slice( 0, 2 );
+			}
+
+			return state.set( action.key, value );
 		default:
 			return state;
 	}
