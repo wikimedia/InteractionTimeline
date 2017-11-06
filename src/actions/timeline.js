48217d18ae901fb8fde01c34cc6df8f7061ec686
Dayllan Maza <dmaza@wikimedia.org>
2017-11-06 10:35:39 -0500

    Add initial logic to get timeline revisions

---
+++ src/actions/timeline.js
@@ -0,0 +1,6 @@
+export function setTimeline( timeline ) {
+	return {
+		type: 'TIMELINE_SET',
+		timeline
+	};
+}
--- src/components/timeline/timeline.container.js
+++ src/components/timeline/timeline.container.js
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux';
+import * as TimelineActions from 'app/actions/timeline';
 import Timeline from './timeline';
 
 export default connect(
