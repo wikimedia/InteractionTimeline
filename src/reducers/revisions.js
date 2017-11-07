ad44164b6b1a6dc955fe52e27718ea5ca32425d6
David Barratt <dbarratt@wikimedia.org>
2017-11-07 00:11:13 -0500

    Remove the revisions on a query update since they are no longer valid

--- src/reducers/revisions.js
+++ src/reducers/revisions.js
@@ -2,6 +2,9 @@ import { OrderedMap } from 'immutable';
 
 export default ( state = new OrderedMap(), action ) => {
 	switch ( action.type ) {
+		case 'QUERY_UPDATE':
+		case 'QUERY_SET_VALUE':
+			return new OrderedMap();
 		case 'REVISIONS_SET':
 			return action.revisions;
 		default:
