17c90cc09c47faf182f985ad88bc28cbbdb8c631
David Barratt <dbarratt@wikimedia.org>
2017-11-13 13:51:39 -0500

    Make timelapse text smaller

--- src/components/timeline/timelapse.js
+++ src/components/timeline/timelapse.js
@@ -5,8 +5,8 @@ import 'material-design-icons/iconfont/material-icons.css';
 const TimelineTimelapse = ( { date } ) => (
 	<div className="row justify-content-center">
 		<div className="col-md-3 col-sm-6 col-10 pb-2 pt-2 border rounded text-center align-text-middle">
-			<div className="d-flex justify-content-center align-items-center">
-				<i className="mr-2 material-icons md-48">timelapse</i>
+			<div className="d-flex small justify-content-center align-items-center">
+				<i className="mr-2 material-icons">timelapse</i>
 				<span>{date}</span>
 			</div>
 		</div>
