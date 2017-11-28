d01f27d7d4529d5df34fd915880b4d9fbec524fd
David Barratt <dbarratt@wikimedia.org>
2017-11-27 21:35:06 -0500

    T180156 Make simpler requests to determine if pages have been edited

--- src/reducers/pages.js
+++ src/reducers/pages.js
@@ -1,29 +1,32 @@
 import { Map } from 'immutable';
+import Page from 'app/entities/page';
 
 export default ( state = new Map(), action ) => {
 	switch ( action.type ) {
-		case 'PAGES_SET_USER_PAGES':
-			return state.set( action.user, action.pages );
-		case 'PAGES_MERGE_USER_PAGES':
-			let pages = state.get( action.user );
-			if ( pages ) {
-				pages = pages.merge( action.pages );
-			} else {
-				pages = action.pages;
-			}
+		case 'PAGES_SET':
+			return action.pages;
+		case 'REVISIONS_SET':
+			const pages = action.revisions.reduce( ( reduction, revision ) => {
 
-			return state.set( action.user, pages );
+				return reduction.set( revision.pageid, new Page( {
+					id: revision.pageid,
+					title: revision.title,
+					editors: new Map( {
+						[ revision.user ]: true
+					} )
+				} ) );
+			}, new Map() );
+
+			return state.mergeDeep( pages );
+		case 'PAGES_UPDATE':
+			return state.mergeDeep( action.pages );
 		case 'QUERY_SET_VALUE':
 			switch ( action.key ) {
-				case 'user':
-					return state.filter( ( value, key ) => !!action.value.get( key ) );
 				case 'wiki':
 					return new Map();
 				default:
 					return state;
 			}
-		case 'QUERY_UPDATE':
-			return state.filter( ( value, key ) => !!action.query.user.has( key ) );
 		default:
 			return state;
 	}
