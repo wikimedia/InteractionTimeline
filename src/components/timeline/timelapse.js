e702b9ee0ea8182e5e7448053fa348f78101f040
David Barratt <dbarratt@wikimedia.org>
2017-11-29 12:53:39 -0500

    Make timebetween smaller

--- src/components/timeline/timelapse.js
+++ src/components/timeline/timelapse.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import 'material-design-icons/iconfont/material-icons.css';
 
 const TimelineTimelapse = ( { date } ) => (
-	<div className="col-md-6 col-12 pb-1 pt-1 box text-center align-text-middle">
-		<div className="d-flex justify-content-center align-items-center">
+	<div className="col-md-6 col-12 box text-center align-text-middle">
+		<div className="d-flex small justify-content-center align-items-center">
 			<i className="mr-2 material-icons">timelapse</i>
 			{/* @TODO Translate "between interactions" */}
 			<span>{date} between interactions</span>
