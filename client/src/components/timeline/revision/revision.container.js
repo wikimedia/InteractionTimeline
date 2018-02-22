b45bdff49d5efb5e1b2e1d6b6f385a0eabaf64d9
David Barratt <dbarratt@wikimedia.org>
2018-02-22 12:37:26 -0500

    T187970 Link to the revision intead of the diff

--- client/src/components/timeline/revision/revision.container.js
+++ client/src/components/timeline/revision/revision.container.js
@@ -1,19 +1,19 @@
 import { connect } from 'react-redux';
 import { toggleDiff } from 'app/actions/diff';
-import { makeGetDiff } from 'app/selectors/diff';
+import { makeGetDiff, makeGetDiffUrl } from 'app/selectors/diff';
 import { makeGetRevisionUrl, makeGetTimestamp } from 'app/selectors/revisions';
 import makeGetSide from 'app/selectors/side';
 import Revision from './revision';
 
 const getSide = makeGetSide();
-const getRevisionUrl = makeGetRevisionUrl();
+const getDiffUrl = makeGetDiffUrl();
 const getTimestamp = makeGetTimestamp();
 const getDiff = makeGetDiff();
 
 export default connect(
 	( state, props ) => ( {
 		side: getSide( state, props ),
-		url: getRevisionUrl( state, props ),
+		url: getDiffUrl( state, props ),
 		timestamp: getTimestamp( state, props ),
 		diff: getDiff( state, props )
 	} ),
