b2c9148b4dd3c2d60e9e8b6d63f643d66efe7337
David Barratt <dbarratt@wikimedia.org>
2018-07-10 19:55:27 -0400

    T197148 Return a message string if all placeholders are strings

---
+++ client/src/components/i18n/message.test.js
@@ -0,0 +1,39 @@
+import IntlProvider from './intl-provider';
+import Message from './message';
+
+test( 'will get a basic message string', () => {
+	// eslint-disable-next-line no-unused-vars
+	const provider = new IntlProvider( {
+		locale: 'en',
+		messages: {
+			en: {
+				test: 'TEST'
+			}
+		}
+	} );
+
+	const text = Message( {
+		id: 'test'
+	} );
+	expect( text ).toStrictEqual( 'TEST' );
+} );
+
+test( 'will get a basic message string with a placeholder', () => {
+	// eslint-disable-next-line no-unused-vars
+	const provider = new IntlProvider( {
+		locale: 'en',
+		messages: {
+			en: {
+				test: 'TEST $1'
+			}
+		}
+	} );
+
+	const text = Message( {
+		id: 'test',
+		placeholders: [
+			'RIGHT'
+		]
+	} );
+	expect( text ).toStrictEqual( 'TEST RIGHT' );
+} );
