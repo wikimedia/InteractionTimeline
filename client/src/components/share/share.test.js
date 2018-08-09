30b308e66f8e28e0a7f91f1c4b0ae3575beb0182
David Barratt <dbarratt@wikimedia.org>
2018-08-09 15:32:08 -0400

    T197148 Fix test failures

--- client/src/components/share/share.test.js
+++ client/src/components/share/share.test.js
@@ -5,12 +5,12 @@ import Share from './share';
 
 test( 'hide button when no results are present', () => {
 	const share = shallow( <Share empty users={[ 'Derby pie', 'Sweets lover' ]} queryString="?user=Derby%20pie&user=Sweets%20lover" /> );
-	expect( share.find( Button ).first().hasClass( 'empty' ) ).toBeTruthy();
+	expect( share.find( Button ).first().hasClass( 'invisible' ) ).toBeTruthy();
 } );
 
 test( 'show button when results are present', () => {
 	const share = shallow( <Share empty={false} users={[ 'Derby pie', 'Sweets lover' ]} queryString="?user=Derby%20pie&user=Sweets%20lover" /> );
-	expect( share.find( Button ).first().hasClass( 'empty' ) ).toBeFalsy();
+	expect( share.find( Button ).first().hasClass( 'visible' ) ).toBeTruthy();
 } );
 
 test( 'clicking on the button opens the modal with selected text', () => {
